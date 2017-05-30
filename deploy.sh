aa70578311bf81bf85dc177c1da54797f9e2a26d
Eli Van fraeyenhove <eli@fenego.be>
2017-05-30 17:06:19 +0200

    fixes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export GRADLE_USER_HOME=$DEPLOYMENT_SOURCE/.gradle
 ./gradlew build
-cp ./build/libs/hello-world-0.0.1.war $DEPLOYMENT_TARGET/webapps
+cp $DEPLOYMENT_SOURCE/build/libs/hello-world-0.0.1.war $DEPLOYMENT_TARGET/webapps
