5ee8c82f574c2f8ff2c07e292febed663284643e
Eli Van fraeyenhove <eli@fenego.be>
2017-05-30 17:03:33 +0200

    fixes

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-echo "PRINTING VAR"
-echo $DEPLOYMENT_TEMP
-echo $DEPLOYMENT_SOURCE
 export GRADLE_USER_HOME=$DEPLOYMENT_SOURCE/.gradle
 ./gradlew build
 cp ./build/libs/hello-world-0.0.1.war $DEPLOYMENT_TARGET/webapps
